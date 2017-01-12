b6b9f33a39b999f68a84e8a27343e01e3dee6ecd
Wesam Gerges <wgerges@modusagency.com>
2017-01-12 17:58:21 -0500

    When the item starts with _ consider it as an attribute to the parent node.

--- src/SoapBox/Formatter/Parsers/Parser.php
+++ src/SoapBox/Formatter/Parsers/Parser.php
@@ -45,73 +45,77 @@ abstract class Parser {
 	}
 
 	/**
-	 * To XML conversion
-	 *
-	 * @param   mixed        $data
-	 * @param   null         $structure
-	 * @param   null|string  $basenode
-	 * @return  string
-	 */
-	private function xmlify($data, $structure = null, $basenode = 'xml') {
-		// turn off compatibility mode as simple xml throws a wobbly if you don't.
-		if (ini_get('zend.ze1_compatibility_mode') == 1) {
-			ini_set('zend.ze1_compatibility_mode', 0);
-		}
+     * To XML conversion
+     *
+     * @param   mixed        $data
+     * @param   null         $structure
+     * @param   null|string  $basenode
+     * @return  string
+     */
+    private function xmlify($data, $structure = null, $basenode = 'xml', $parent=null) {
+        // turn off compatibility mode as simple xml throws a wobbly if you don't.
+        if (ini_get('zend.ze1_compatibility_mode') == 1) {
+            ini_set('zend.ze1_compatibility_mode', 0);
+        }
+        $flag=false;
+        if ($structure == null) {
+            $structure = simplexml_load_string("<?xml version='1.0' encoding='utf-8'?><$basenode />");
+        }
 
-		if ($structure == null) {
-			$structure = simplexml_load_string("<?xml version='1.0' encoding='utf-8'?><$basenode />");
-		}
+        // Force it to be something useful
+        if (!is_array($data) && !is_object($data)) {
+            $data = (array) $data;
+        }
 
-		// Force it to be something useful
-		if (!is_array($data) && !is_object($data)) {
-			$data = (array) $data;
-		}
+        foreach ($data as $key => $value) {
+            
+            // convert our booleans to 0/1 integer values so they are
+            // not converted to blanks.
+            if (is_bool($value)) {
+                $value = (int) $value;
+            }
 
-		foreach ($data as $key => $value) {
-			// convert our booleans to 0/1 integer values so they are
-			// not converted to blanks.
-			if (is_bool($value)) {
-				$value = (int) $value;
-			}
+            // no numeric keys in our xml please!
+            if (is_numeric($key)) {
+                // make string key...
+                $flag = true;
+                $key = (Str::singular($basenode) != $basenode) ? Str::singular($basenode) :  $structure->getName();                
+            }
+            
+            // replace anything not alpha numeric
+            $key = preg_replace('/[^a-z_@\-0-9]/i', '', $key);
 
-			// no numeric keys in our xml please!
-			if (is_numeric($key)) {
-				// make string key...
-				$key = (Str::singular($basenode) != $basenode) ? Str::singular($basenode) : 'item';
-			}
-
-			// replace anything not alpha numeric
-			$key = preg_replace('/[^a-z_@\-0-9]/i', '', $key);
-
-			// if there is another array found recrusively call this function
-			if (is_array($value) or is_object($value)) {
-				
-				if($key == "@attributes"){
-					foreach($value as $k => $v)					
-						$structure->addAttribute(str_replace('@', '', $k ), $v);
-				}
-				// recursive call if value is not empty
-				else {
-						$node = $structure->addChild($key);
-					 	if (!empty($value)) {
-							$this->xmlify($value, $node, $key)	;
-					 	}
-				}
-
-			} else {
-				// add single node.
-				$value = htmlspecialchars(html_entity_decode($value, ENT_QUOTES, 'UTF-8'), ENT_QUOTES, "UTF-8");	
-				if(substr($key, 0, 1) == "@"){
-					$structure->addAttribute(str_replace('@', '', $key ), $v);
-				} else {
-					$structure->addChild($key, $value);
-				}	
-			}
-		}
-
-		// pass back as string. or simple xml object if you want!
-		return $structure->asXML();
-	}
+            // if there is another array found recrusively call this function
+            if (is_array($value) or is_object($value)) {
+                
+                if($key == "@attributes"){
+                    foreach($value as $k => $v)                    
+                        $structure->addAttribute(str_replace('@', '', $k ), $v);
+                }
+                // recursive call if value is not empty
+                else {
+                    if(!$flag)
+                        $node = $structure->addChild($key);
+                    else
+                        $node = $parent->addChild($key);
+                    if (!empty($value)) {
+                        $this->xmlify($value, $node, $key, $structure);                        
+                    }                    
+                }
+                
+            } else {
+                // add single node.
+                $value = htmlspecialchars(html_entity_decode($value, ENT_QUOTES, 'UTF-8'), ENT_QUOTES, "UTF-8");    
+                if(substr($key, 0, 1) == "_"){
+                    $structure->addAttribute(str_replace('_', '', $key ), $value);
+                } else {
+                    $structure->addChild($key, $value);
+                }    
+            }
+        }
+        // pass back as string. or simple xml object if you want!
+        return $structure->asXML();
+    }
 
 	/**
 	 * Return an xml representation of the data stored in the parser
